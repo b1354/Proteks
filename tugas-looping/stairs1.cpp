3bbd951404af60500ef1dfeef4d6a47606f61ccb
b1354 <bayurizkyseptiansyah@gmail.com>
2023-05-12 16:11:58 +0700

    tugas loop

---
+++ tugas-looping/stairs1.cpp
@@ -0,0 +1,20 @@
+
+// Online C++ Compiler - Build, Compile and Run your C++ programs online in your favorite browser
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int panjang_segitiga;
+    cout << "masukan panjang segitiga: ";
+    cin >> panjang_segitiga;
+    
+    for (int i=0; i < panjang_segitiga; i++) {
+        for (int j=0; j <= i; j++) {
+            cout << "0 ";
+        }
+        cout << endl;
+    }
+}
